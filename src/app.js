dd6619e4c9938bea85a98173d15745137d8518c3
thayron <thayronf3@gmail.com>
2020-10-27 06:09:11 -0300

    challenge GoStack bootCamp is complete

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 
-// const { v4: uuid } = require('uuid');
+const { v4: uuid } = require('uuid');
 
 const app = express();
 
@@ -11,23 +11,66 @@ app.use(cors());
 const repositories = [];
 
 app.get("/repositories", (request, response) => {
-  // TODO
+  return response.json(repositories)
 });
 
 app.post("/repositories", (request, response) => {
-  // TODO
+  const { title, url, techs, likes } = request.body;
+  const repositori = {
+    id: uuid(),
+    title,
+    url,
+    techs,
+    likes: 0
+  }
+  repositories.push(repositori)
+  return response.status(200).json(repositori)
+
 });
 
 app.put("/repositories/:id", (request, response) => {
-  // TODO
+  const { id } = request.params;
+  const repositoriIndice = repositories.findIndex(repositori => repositori.id === id)
+  if (repositoriIndice < 0) {
+    return response.status(400).json({ error: "repositorie not found" })
+  }
+
+  const { url, title, techs } = request.body;
+  const repositorie = {
+    id,
+    url,
+    title,
+    techs,
+    likes: repositories[repositoriIndice].likes,
+
+  }
+  repositories[repositoriIndice] = repositorie;
+  return response.status(200).json(repositorie)
+
 });
 
 app.delete("/repositories/:id", (request, response) => {
-  // TODO
+  const { id } = request.params;
+  const repositoriIndice = repositories.findIndex(repositori => repositori.id === id)
+  if (repositoriIndice < 0) {
+    return response.status(400).json({ error: "repositorie not foind" })
+  }
+
+  repositories.splice(repositoriIndice, 1);
+  return response.status(204).send()
 });
 
 app.post("/repositories/:id/like", (request, response) => {
-  // TODO
+  const { id } = request.params;
+
+  const repositoriIndice = repositories.findIndex(repositori => repositori.id === id)
+  if (repositoriIndice < 0) {
+    return response.status(400).json({ error: "repository not foind" })
+  }
+
+  repositories[repositoriIndice].likes += 1;
+  return response.json(repositories[repositoriIndice])
+
 });
 
 module.exports = app;
